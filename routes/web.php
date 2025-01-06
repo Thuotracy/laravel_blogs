d574b3e3b262da8174464befaba725ee5292f045
Thuotracy <tracyjacobs775@gmail.com>
2025-01-06 13:26:21 +0300

    created editing blogs functionality

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,6 @@ Route::post('/login', [UsermodelController::class, 'login']);
 
 
 Route::post('/create-post', [PostController::class, 'createPost']);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'editPost']);
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
